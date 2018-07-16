893f8d34825876a35debf2338a6cda270455196b
VJVasiljev <v.j.vasiljev@gmail.com>
2018-07-16 12:06:22 +0300

    add generated names, random imgs

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="Robot" src="https://robohash.org/Slava?200x200" />
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+      <img alt="Robot" src={`https://robohash.org/${id * Math.random()}?200x200`} />
       <div>
-        <h2>Bender</h2>
-        <p>bender@futurama.done</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
