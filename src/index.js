42b2ed18cca902203c2b2a922613dd005a53b2f9
VJVasiljev <v.j.vasiljev@gmail.com>
2018-07-16 11:32:15 +0300

    add Hello component, add tachyons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Hello' + 'React Ninja'}/>, document.getElementById('root'));
 registerServiceWorker();
