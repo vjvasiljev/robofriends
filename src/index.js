288179f4fec9cb7ce6062d370899ed8717549491
VJVasiljev <v.j.vasiljev@gmail.com>
2018-07-17 20:50:17 +0300

    add card implementation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
+import CardList from './CardList';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 import { robots } from './robots';
