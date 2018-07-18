2c0c8d35e89a8c8481371b55b29378aa6541919e
VJVasiljev <v.j.vasiljev@gmail.com>
2018-07-19 01:43:05 +0300

    remove manual error throw in cardList

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error('Nooooooo!');
-  }
+  // if (true) {
+  //   throw new Error('Nooooooo!');
+  // }
   const cardComponent = robots.map((user, i) => {
     return (<Card
       key={i}
