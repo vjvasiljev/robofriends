06fb1b0d395f46c4f3e3e01e9f49d2a80e563602
VJVasiljev <v.j.vasiljev@gmail.com>
2018-07-19 14:59:32 +0300

    complete redux implementation

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,34 +6,35 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField } from '../actions';
+import { setSearchField, requestRobots } from '../actions';
 
 const mapStateToProps = state => {
   return {
-    searchField: state.searchField
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
   }
 }
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      // searchfield: ''
-    }
-  }
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     // searchfield: ''
+  //   }
+  // }
 
   componentDidMount() {
-    // console.log(this.props.store.getState());
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
+    this.props.onRequestRobots();
   }
 
   // onSearchChange = (event) => {
@@ -41,12 +42,11 @@ class App extends Component {
   // }
 
   render() {
-    const { robots } = this.state;
-    const { searchField, onSearchChange } = this.props;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
-    return (!robots.length) ?
+    return isPending ?
       <h1 className="tc">Loading...</h1> :
       (<div className="tc">
         <h1 className="f2">RoboFriends</h1>
