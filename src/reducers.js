8a9e25a4205671b8ad40f9cc74c1d23bbd92418e
VJVasiljev <v.j.vasiljev@gmail.com>
2018-07-19 12:26:10 +0300

    add Redux Provider

--- src/reducers.js
+++ src/reducers.js
@@ -6,7 +6,7 @@ const initialState = {
 
 export const searchRobots = (state = initialState, action = {}) => {
   switch (action.type) {
-    case: CHANGE_SEARCHFIELD:
+    case CHANGE_SEARCHFIELD:
       return Object.assign({}, state, { searchField: action.payload });
     default:
       return state;
